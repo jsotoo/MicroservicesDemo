31c72704501567a113c2b519e675d49565fa9894
Erick Aróstegui Cunza <erick.arostegui.cunza@gmail.com>
2024-09-07 09:40:16 -0500

    Session 06

---
+++ 06 - Microservices/Microservices.Monitoring/Microservices.Monitoring.Sales.API/Infrastructure/Log/ICustomLogEnricher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservices.Monitoring.Sales.API.Infrastructure.Log
+{
+    public interface ICustomLogEnricher
+    {
+        string CorrelationId { get; set; }
+    }
+}
